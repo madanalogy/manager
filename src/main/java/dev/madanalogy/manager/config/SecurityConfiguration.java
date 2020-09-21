355c8ef6a9d97fd03f8423f813265d9068cbce44
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-09-22 01:31:19 +0800

    Add unauthenticated api access for items

--- src/main/java/dev/madanalogy/manager/config/SecurityConfiguration.java
+++ src/main/java/dev/madanalogy/manager/config/SecurityConfiguration.java
@@ -117,6 +117,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/manager-kafka/publish").permitAll()
+            // TODO: Remove after Task B1 assessed
+            .antMatchers("/api/items").permitAll()
             .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
